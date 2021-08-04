3a2c42c0985817523e5555cc407d936b29448b19
leticia <letisaless@gmail.com>
2021-08-04 10:44:35 -0300

    create simple client and server

---
+++ trabalho/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import socket
+import sys
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 65432 # Port to listen on
+
+if PORT > 1023: print('Using port', PORT)
+else: sys.exit('Please enter a valid port (non-privileged ports are > 1023)')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try: s.bind((HOST, PORT))
+    except: sys.exit('Error during port alloc')
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
