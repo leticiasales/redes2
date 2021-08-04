b18b29a3d515c1550062fafb011bc0396cd58a80
leticia <letisaless@gmail.com>
2021-08-04 11:18:58 -0300

    read message

--- trabalho/client.py
+++ trabalho/client.py
@@ -6,8 +6,13 @@ HOST = sys.argv[1] if len(sys.argv) > 2 else '127.0.0.1'  # The server's hostnam
 PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 65432 # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+  s.connect((HOST, PORT))
 
-print('Received', repr(data))
+  message = sys.argv[3] if len(sys.argv) > 2 \
+    else sys.argv[1] if len(sys.argv) > 1 \
+    else input("Enter message: ")
+
+  s.sendall(bytes(message, 'utf-8'))
+  data = s.recv(1024)
+
+  print('Received', data.decode('utf-8'))
