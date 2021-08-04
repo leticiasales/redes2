d7c23e181b5896e9467897f8bf299ac2c36bd8d8
leticia <letisaless@gmail.com>
2021-08-04 15:35:39 -0300

    use encryption

--- trabalho/client.py
+++ trabalho/client.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
+import json
 import socket
 import sys
+
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+from diffieHellman import *
+
+BLOCK_SIZE = 32
 
 HOST = sys.argv[1] if len(sys.argv) > 2 else '127.0.0.1'  # The server's hostname or IP address
 PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 65432 # The port used by the server
 
+private_key = rand_prime(1024)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
 
-  message = sys.argv[3] if len(sys.argv) > 2 \
-    else sys.argv[1] if len(sys.argv) > 1 \
-    else input("Enter message: ")
+  first_message = s.recv(1024)
+  print(first_message)
 
-  s.sendall(bytes(message, 'utf-8'))
   data = s.recv(1024)
+  jsonData = json.loads(data.decode())
 
-  print('Received', data.decode('utf-8'))
+  base = jsonData['base']
+  shared_key = jsonData['shared_key']
+  server_public_secret = jsonData['public_secret']
+
+  public_secret = generate_public_secret(base, private_key, shared_key)
+  shared_secret = generate_shared_secret(server_public_secret, private_key, shared_key)
+
+  s.send(bytes(json.dumps({
+    'public_secret': public_secret
+  }), 'utf-8'))
+
+  cipher = DES.new(shared_secret.to_bytes(8, byteorder='big'), DES.MODE_ECB)
+
+  while True:
+    message = input("Enter message: ")
+
+    if message == 'exit':
+      exit()
+    else:
+      s.sendall(cipher.encrypt(pad(bytes(message, 'utf-8'), BLOCK_SIZE)))
--- trabalho/server.py
+++ trabalho/server.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
+import json
 import socket
 import sys
+
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+from diffieHellman import *
+
+BLOCK_SIZE = 32
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 65432 # Port to listen on
 
 if PORT > 1023: print('Using port', PORT)
 else: sys.exit('Please enter a valid port (non-privileged ports are > 1023)')
+
+base, private_key, shared_key = rand_prime(255), rand_prime(255), rand_prime(255)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   try: s.bind((HOST, PORT))
@@ -15,11 +24,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.listen()
   print('Awaiting client connection...')
 
-  conn, addr = s.accept()  
+  conn, (client_host, client_port) = s.accept()  
+
   with conn:
-    print('Connected by', addr)
+    print(f'Connected by {client_host}:{client_port}')
+
+    conn.send(bytes('Beginning Diffie-Hellman key exchange.', 'utf-8'))
+
+    public_secret = generate_public_secret(base, private_key, shared_key)
+
+    conn.send(bytes(json.dumps({
+      'base': base,
+      'shared_key': shared_key,
+      'public_secret': public_secret
+      }), 'utf-8'))
+
+    data = conn.recv(1024)
+
+    client_public_secret = json.loads(data.decode())['public_secret']
+    shared_secret = generate_shared_secret(client_public_secret, private_key, shared_key)
+    cipher = DES.new(shared_secret.to_bytes(8, byteorder='big'), DES.MODE_ECB)
+
     while True:
       data = conn.recv(1024)
+
       if not data:
         break
-      conn.sendall(data)
+        conn.sendall(data)
+      else:
+        decrypted_data = unpad(cipher.decrypt(data), BLOCK_SIZE).decode('utf-8')
+
+        print(f'Received message: \"{decrypted_data}\"')
